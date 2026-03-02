508aba53f4b36a8a272d1c5034d9e2bd933e26f1
Bolol4002 <sayoojsumesh1112@gmail.com>
2026-03-02 10:45:27 +0530

    exp 13 opps updated

--- OOPS/Exp01DecisionLoops.java
+++ OOPS/Exp01DecisionLoops.java
@@ -1,13 +1,11 @@
 public class Exp01DecisionLoops {
     public static void main(String[] args) {
         int n = 7;
-
         if (n % 2 == 0) {
             System.out.println("even");
         } else {
             System.out.println("odd");
         }
-
         int sum = 0;
         for (int i = 1; i <= n; i++) {
             sum += i;
